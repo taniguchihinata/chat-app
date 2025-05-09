d1c13b736887de4ff12baca3d6fcd6a73aebff3f
taniguchihinata <taniguchi_hinata@rext.work>
2025-05-09 09:54:07 +0900

    データベースを作れた。見れるかは不明

--- frontend/pages/src/App.js
+++ frontend/pages/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>新規登録</h1>
+      <h4>ユーザー名</h4>
+      <textbox></textbox>
+      <button>登録</button>
     </div>
   );
 }
